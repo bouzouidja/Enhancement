a132123fa0ad5ba72d9c6c3733a156ca705d99ca
bouzouidja <bouzouidjasidahmed@gmail.com>
2022-01-08 23:54:34 +0100

    Last update

--- convolve_filter.py
+++ convolve_filter.py
@@ -5,57 +5,48 @@ from skimage.filters.rank import median, mean
 from matplotlib import pyplot as plt
 import numpy as np
 from skimage.morphology import square, disk
+from numpy.fft import fft2,ifft2,fftshift,ifftshift
 
-
-
-
-
-
-
-
-def median_filter(image ) : 
+#Convolution in spatial space
+def median_filter(image): 
 	im_output = image.copy()
 
 	if len(image.shape)>2	:
 		# apply the median filter for each channel
-		out_median_red = median(im_output[:,:,0], disk(5))
-		out_median_green = median(im_output[:,:,1], disk(5))
-		out_median_blue = median(im_output[:,:,2], disk(5))
+		out_median_red = median(im_output[:,:,0], disk(4))
+		out_median_green = median(im_output[:,:,1], disk(4))
+		out_median_blue = median(im_output[:,:,2], disk(4))
 
 		# after filtering with median, i notice that the output shape will be bigger then the image input, 
 		# for avoiding shape error widh and heigh, i create a new image with the same shape from one of the mean channel output
 		out_median = np.zeros((out_median_red.shape[0],out_median_red.shape[1],3))
 		# median
-		out_median[:,:,0] = out_median_red
-		out_median[:,:,1] = out_median_green
-		out_median[:,:,2] = out_median_blue
+		out_median[:, :, 0] = out_median_red
+		out_median[:, :, 1] = out_median_green
+		out_median[:, :, 2] = out_median_blue
 		out = out_median
 		imsave('RGB_denoised_image.jpg', out)
 	else : 
-		out = median(im_output , disk(5))
+		out = median(im_output , disk(4))
 		imsave('Gray_denoised_image.jpg', out)
 	
 	return out
 
-
-
-
-
-def mean_filter(image ) : 
+def mean_filter(image) : 
 	im_output = image.copy()
 	if len(image.shape)>2	:
 	# apply the mean filter for each channel
-		out_mean_red = mean(im_output[:,:,0], disk(5))
-		out_mean_green = mean(im_output[:,:,1], disk(5))
-		out_mean_blue = mean(im_output[:,:,2], disk(5))
+		out_mean_red = mean(im_output[:,:,0], disk(3))
+		out_mean_green = mean(im_output[:,:,1], disk(3))
+		out_mean_blue = mean(im_output[:,:,2], disk(3))
 
 	# after filtering with mean, i notice that the output shape will be bigger then the image input,
 	# for avoiding shape error widh and heigh, i create a new image with the same shape from one of the mean channel output 
 		out_mean = np.zeros((out_mean_red.shape[0],out_mean_red.shape[1],3))
 		# median
-		out_mean[:,:,0] = out_mean_red
-		out_mean[:,:,1] = out_mean_green
-		out_mean[:,:,2] = out_mean_blue
+		out_mean[:, :, 0] = out_mean_red
+		out_mean[:, :, 1] = out_mean_green
+		out_mean[:, :, 2] = out_mean_blue
 		out = out_mean
 		imsave('RGB_denoised_image.jpg', out)
 	else : 
@@ -63,22 +54,13 @@ def mean_filter(image ) :
 		imsave('Gray_denoised_image.jpg', out)
 	return out
 
-
-
-
-
-def Gaussian_filter(image ,level):
+def gaussian_filter(image, level):
 	im_output = image.copy()
-
 	if len(image.shape)> 2:
 		out =  gaussian(im_output.astype('uint8'), sigma=int(level) , multichannel=True)
-		#imsave('Blured_image.jpg', out)
 	else :
-		out =  gaussian(image, sigma = int(level))
-		#imsave('Blured_image.jpg', out)
+		out =  gaussian(image, sigma=int(level))
 	return  out
-
-
 
 def cumul_hist(image):
 
@@ -87,18 +69,52 @@ def cumul_hist(image):
 	for v in range(256):
 		c+= (v==image).sum()
 		cumul_hist[v] = c 
-	itemindex = np.where(cumul_hist==cumul_hist.max())
-	itemindex2 = np.where(cumul_hist==cumul_hist.min())
 	cumul_hist/= cumul_hist.max()
 	
 	return cumul_hist
 
-
-
-def homogeneous_contrast_image(image):  #### or auto leveling
+def homogeneous_contrast_image(image):  
+	"""This method is to perform equalization, it used to brightening the contrast of the image
+		- This operation not reversible because we have changed the values of pixels (loosing information)
+		 during the processing.
+		- 
+	"""
 	histo = cumul_hist(image)
 	lut = (histo*255).astype('uint8')
 	im_out = lut[image]
 	imsave('Homogeneous_image.jpg', im_out)
 
-	return im_out
+	return im_out, histo
+
+## convolution in Fourier space
+def fourier(im):
+	return fftshift(fft2(im))
+
+def ifourier(f):
+	return ifft2(ifftshift(f)).real
+
+def get_disk_mask(r, imshape):
+	a, b = imshape[0]//2, imshape[1]//2   
+	y,x = np.ogrid[-a:imshape[0]-a, -b:imshape[1]-b]
+	mask = x*x + y*y <= r*r
+	return mask
+
+def prepare_mask(image,radius):
+	mask = np.zeros(image.shape)
+	d = disk(radius)
+	mask[mask.shape[0]//2-d.shape[0]//2:mask.shape[0]//2+d.shape[0]//2+1,
+	 mask.shape[1]//2-d.shape[1]//2:mask.shape[1]//2+d.shape[1]//2+1] = d
+	return mask
+
+def high_pass(image,r):
+	f = fourier(image)
+	mask = get_disk_mask(r,f.shape)
+	f2 = f*(1-mask)
+	return ifourier(f2)
+
+def low_pass(image,r):
+	f = fourier(image)
+	mask = prepare_mask(image,r)
+	f2 = f*mask
+	amplitude = np.sqrt(f2.real**2 + f2.imag**2)
+	return ifourier(f2),amplitude
--- enhancement.py
+++ enhancement.py
@@ -29,9 +29,7 @@ while True :
 print(" \n \n\t \t \tEnhancement options \n")
 while True:    
     
-    choice  = input("If you have an image with salt and pepper noise enter 'D' Denoising...\nEnter 'B' for bluring the image... \n Enter 'H' for homogenize contrast image... \n" )    
-    
-
+    choice  = input("Enter 'D' for denoising salt and pepper image...\n Enter 'H' for homogenize contrast image... \n Enter 'B' for bluring the image... \n" )
     if choice != "D" and choice != "B" and choice != "H" : 
         print ("Your input is not the correct value:")
     elif  choice == "B":
@@ -47,76 +45,54 @@ while True:
 
     
     else :
-        print("Your input is right" ,choice)
+        print("Your input is")
         break    
 
 
-plt.figure()
-plt.subplot(1,2,1)
-plt.title("Originale image")
 
-
-
-
-
-if len(img.shape)>2:
-
-
-    plt.imshow(img )
-    options = {"D"  : convfl.median_filter ,
-           "B"      : convfl.Gaussian_filter,
+# Applying filter according to option choosed
+options = {"D"  : convfl.median_filter,
+           "B"      : convfl.gaussian_filter,
            "H"      : convfl.homogeneous_contrast_image}
     
 
-    plt.subplot(1,2,2)
-    if choice =="B" :
-        result = options["B"](img , level)
-        plt.imshow(result)
-        plt.title("Blur image ")
-    else : 
-        result = options[choice](img )
-    
-        if choice == "D":
-
-            plt.imshow(result.astype('uint8'))
-            plt.title("Denoised RGB image ")
-        elif choice == "H" :
-            plt.imshow(result)
-            plt.title("Homogeneous contrast image ")
-
-    
-
-else : 
-
-    plt.imshow(img, cmap=plt.cm.gray )
-    options = {"D"  : convfl.median_filter ,
-           "B"      : convfl.Gaussian_filter,
-           "H"      : convfl.homogeneous_contrast_image}
-    plt.subplot(1,2,2)
-    if choice =="B" : 
-        result = options["B"](img , level)
-        plt.imshow(result ,cmap=plt.cm.gray)
-        plt.title("Blur image ")
-    else :
-
-        result = options[choice](img )
-    
-        if choice == "D":
-            plt.imshow(result ,cmap=plt.cm.gray)
-            plt.title("Denoised grayscale image ")
+if choice =="B" :
+        plt.figure(figsize=(15,10))
+        plt.subplot(1,2,1)
+        plt.title("Originale image")
+        plt.imshow(img,cmap=plt.cm.gray)
+        #result,amplitude = options["B"](img, 50)
+        result = options["B"](img, level)
+        plt.subplot(1,2,2)
+        plt.title("Filtred image")
+        plt.imshow(result, interpolation='none', cmap=plt.cm.gray)
+        #plt.subplot(1,3,3)
+        #plt.imshow(amplitude, interpolation='none', cmap='viridis')
+        #plt.title('Fourier amplitude')
+        #plt.axis('off')
+elif choice == "D":
+            plt.figure(figsize=(15,10))
+            plt.subplot(1,2,1)
+            plt.title("Originale image")
+            plt.imshow(img, cmap=plt.cm.gray)
+            plt.subplot(1,2,2)
+            result = options["D"](img)
+            plt.title("Denoised image")
+            plt.imshow(result.astype('uint8'), cmap=plt.cm.gray)
             
-        elif choice == "H" :
-            plt.imshow(result , cmap =plt.cm.gray)
-            plt.title("Homogeneous contrast image ")
-
-
+            
+elif choice == "H":
+            plt.figure(figsize=(15,10))
+            plt.subplot(1,2,1)
+            plt.title("Originale image")
+            plt.imshow(img)
+            result, cumul_histo = options["H"](img)
+            plt.subplot(1,2,2)
+            plt.title("Homogeneous contrast image")
+            plt.imshow(result)
+            #plt.subplot(1,3,3)
+            #plt.plot(cumul_histo)
+            #plt.title("Cumulative histogram")
 
 plt.show()
-
-
-
-
-
-
-
 print("END....")
